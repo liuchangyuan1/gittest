1223f6100e821391935411c620fbe969fef9ecf2
liuchangyuan1 <386728523@qq.com>
2016-08-02 12:48:26 +0800

    test

---
+++ app/src/main/java/com/example/liucy01/getuidemo/PushDemoReceiver.java
@@ -0,0 +1,15 @@
+package com.example.liucy01.getuidemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by liucy01 on 2016/7/27.
+ */
+public class PushDemoReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
